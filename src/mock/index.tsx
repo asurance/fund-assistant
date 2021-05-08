2026eb0ef9067f092836ced478554e128776c18a
asurance <784672616@qq.com>
2021-05-08 22:12:45 +0800

    调整脚本缓存方式

--- src/mock/index.tsx
+++ src/mock/index.tsx
@@ -1,7 +1,27 @@
 import { render } from 'react-dom'
 import React from 'react'
 import App from '../email'
-import data from '../../data.json'
+
+const data = [
+  {
+    date: 1620172800000,
+    middlePETTM: 30.65,
+    averagePETTM: 43.94,
+    middlePELYR: 35.28,
+    averagePELYR: 50.67,
+    close: 5123.49,
+    marketId: '5',
+  },
+  {
+    date: 1620259200000,
+    middlePETTM: 30.82,
+    averagePETTM: 43.79,
+    middlePELYR: 35.36,
+    averagePELYR: 50.76,
+    close: 5061.12,
+    marketId: '5',
+  },
+]
 
 const fund = [-1.5]
 
