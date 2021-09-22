c3e9086469ae40d8b85f18fee0d3845aa1dbabe5
asurance <784672616@qq.com>
2021-09-22 14:13:04 +0800

    直接通过接口获取基金数据

--- src/utils/fund.ts
+++ src/utils/fund.ts
@@ -1,23 +1,62 @@
+import moment from 'moment'
 import { FundInfoMap } from '../config'
-import { Search, useBrowser } from './uses'
+import { Request } from './uses'
 
 export async function GetFundPrice(): Promise<Map<string, number[]>> {
-  return await useBrowser(async (browser) => {
-    const out = new Map<string, number[]>()
-    const promiseList: Promise<void>[] = []
-    for (const [name, { code }] of FundInfoMap) {
-      promiseList.push(
-        Search(browser, code)
-          .then((price) => {
-            out.set(name, price)
-          })
-          .catch((err) => {
-            out.set(name, [])
-            console.error(err)
-          }),
-      )
+  const out = new Map<string, number[]>()
+  const promiseList: Promise<void>[] = []
+  for (const [name, { code }] of FundInfoMap) {
+    promiseList.push(
+      GetSinglePrice(code)
+        .then((price) => {
+          out.set(name, price)
+        })
+        .catch((err) => {
+          out.set(name, [])
+          console.error(err)
+        }),
+    )
+  }
+  await Promise.all(promiseList)
+  return out
+}
+
+const timestamp = moment().format('YYYYMMDDHHmmss')
+
+async function GetSinglePrice(code: string): Promise<number[]> {
+  const [historical, predict] = await Promise.all([
+    GetNetWorthTrend(code),
+    GetPredict(code),
+  ])
+  if (predict === null || historical.length === 0) {
+    return []
+  } else {
+    historical.push(predict)
+    return historical.reverse()
+  }
+}
+
+async function GetNetWorthTrend(code: string): Promise<number[]> {
+  const jsScript = await Request<string>(
+    `http://fund.eastmoney.com/pingzhongdata/${code}.js?v=${timestamp}`,
+  )
+  const netWorthTrend = eval(`${jsScript};Data_netWorthTrend`) as {
+    equityReturn: number
+  }[]
+  return (netWorthTrend ?? []).map((trend) => trend.equityReturn)
+}
+
+async function GetPredict(code: string): Promise<number | null> {
+  const jsScript = await Request<string>(
+    `http://fundgz.1234567.com.cn/js/${code}.js?rt=${moment.now()}`,
+  )
+  return await new Promise<number | null>((resolve) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const jsonpgz = (data: { gszzl: string }) => {
+      const value = parseFloat(data.gszzl)
+      resolve(isNaN(value) ? null : value)
     }
-    await Promise.all(promiseList)
-    return out
+    eval(jsScript)
+    resolve(null)
   })
 }
